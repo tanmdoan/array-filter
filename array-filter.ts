917827074517de70865314d12c35d4297eedc5ed
Tan Doan <tdoan@vmware.com>
2023-11-12 12:16:58 -0700

    initial

---
+++ array-filter.ts
@@ -0,0 +1,9 @@
+/**
+ * @template T
+ * @param { (value: T, index: number, array: Array<T>) => boolean } callbackFn
+ * @param {any} [thisArg]
+ * @return {Array<T>}
+ */
+Array.prototype.myFilter = function (callbackFn, thisArg) {
+  throw 'Not implemented!';
+};
